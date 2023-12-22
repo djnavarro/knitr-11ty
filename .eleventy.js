b0277e48bbad388d0d8ea80e513b73785c50676e
Danielle Navarro <djnavarro@protonmail.com>
2023-12-23 09:26:15 +1100

    adds css styling

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy("normalize.css");
+  eleventyConfig.addPassthroughCopy("skeleton.css");
 };
